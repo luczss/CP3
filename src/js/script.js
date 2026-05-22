6fa1da793faa14598819a282fc6e3040f0bff1a4
lucas <luczssz3@gmail.com>
2026-05-22 14:31:18 -0300

    feat:produtos feitos

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+// ── PRODUTOS ─────────────────────────────────────────────────────────────────
+const produtos = [
+  { 
+    id:1, 
+    modelo:"Retrô",
+    nome:"Maeving RM1",    
+    descricao:"Café racer elétrica com design retrô e tecnologia moderna.", 
+    preco:60000, 
+    img:"" 
+},
+  { 
+    id:2, 
+    modelo:"Moto Cross",      
+    nome:"Sur-Ron Light Bee",  
+    descricao:"Moto off-road leve e potente para trilhas e terrenos difíceis.", 
+    preco:35000, 
+    img:"" 
+},
+  { 
+    id:3, 
+    modelo:"Urbana", 
+    nome:"Voltz EV1",  
+    descricao:"Scooter elétrica urbana, econômica e perfeita para o dia a dia na cidade.", 
+    preco:25000, 
+    img:"" 
+},
+  { 
+    id:4, 
+    modelo:"Sport",   
+    nome:"Zero SR/F", 
+    descricao:"Moto elétrica esportiva com aceleração forte e visual moderno.", 
+    preco:120000, 
+    img:"" 
+},
+  { 
+    id:5, 
+    modelo:"Adventure",   
+    nome:"Zero DSR/X",  
+    descricao:"Modelo adventure ideal para viagens, estradas e trilhas.", 
+    preco:150000, 
+    img:"" 
+}
+];
